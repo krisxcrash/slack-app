3b9557a2478edae093d3927b6df0606d9be92846
Kristine Martin <kristinenicolemartin@gmail.com>
2019-03-20 20:09:07 -0700

    updated to add messages section

--- slack-app/src/components/App.js
+++ slack-app/src/components/App.js
@@ -1,10 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
 import "./App.css";
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+import { connect } from "react-redux";
 
-export default App;
+const App = ({ currentUser, currentChannel }) => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel key={currentUser && currentUser.id} currentUser={currentUser} />
+    <Grid.Column style={{ marginLeft: "320px" }}>
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+      />
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel
+});
+
+export default connect(mapStateToProps)(App);
--- slack-app/src/index.js
+++ slack-app/src/index.js
@@ -15,16 +15,32 @@ import {
   withRouter
 } from "react-router-dom";
 
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers/index";
+
+import { setUser, clearUser } from "./actions";
+import Spinner from "./Spinner";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        this.props.setUser(user);
         this.props.history.push("/");
+      } else {
+        this.props.history.push("/login");
+        this.props.clearUser();
       }
     });
   }
   render() {
-    return (
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
@@ -34,12 +50,23 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+  connect(
+    mapStateFromProps,
+    { setUser, clearUser }
+  )(Root)
+);
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
