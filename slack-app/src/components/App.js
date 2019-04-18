7405314b247f81b76dee47d75d5a4ef34a550475
Kristine Martin <kristinenicolemartin@gmail.com>
2019-04-18 11:15:55 -0700

    updated top posters section of metapanel

--- slack-app/src/components/App.js
+++ slack-app/src/components/App.js
@@ -8,7 +8,7 @@ import MetaPanel from "./MetaPanel/MetaPanel";
 
 import { connect } from "react-redux";
 
-const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
     <SidePanel key={currentUser && currentUser.id} currentUser={currentUser} />
@@ -23,6 +23,7 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
     <Grid.Column width={4}>
       <MetaPanel
         key={currentChannel && currentChannel.id}
+        userPosts={userPosts}
         currentChannel={currentChannel}
         isPrivateChannel={isPrivateChannel}
       />
@@ -33,7 +34,8 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
-  isPrivateChannel: state.channel.isPrivateChannel
+  isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts
 });
 
 export default connect(mapStateToProps)(App);
